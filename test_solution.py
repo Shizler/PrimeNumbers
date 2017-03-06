c40c1ca90c725102e3fbe98474fe98767aa41012
Shizler <win99nie@gmail.com>
2017-03-06 12:25:23 +0000

    checks for non-intergers

---
+++ test_solution.py
@@ -0,0 +1,6 @@
+import unittest 
+class TestSolution(unittest.TestCase):
+	def check_if_number_is_an_interger(self):
+		numbers=prime_numbers("8")
+		self.assertTrue("This is not an interger")
+		pass
