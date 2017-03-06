dca36782539add16ffcfa4da9ba02eb2652a056a
Shizler <win99nie@gmail.com>
2017-03-06 12:40:25 +0000

    if numbers is one or zero

---
+++ testCase2.py
@@ -0,0 +1,6 @@
+import unittest 
+class TestSolution(unittest.TestCase):
+	def check_if number_is_one_or_zero(self):
+		numbers=prime_numbers(0,1)
+		self.assertTrue("This is not a prime number")
+		pass
